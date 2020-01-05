07ce97ba8f8c074abb7cff3704160e0d729691bd
Umran Hussein <umran.hussein@gmail.com>
2020-01-05 03:57:19 -0800

    refactored pubkey and privkey into their own files

---
+++ privkey.go
@@ -0,0 +1,65 @@
+package crypto
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+// PrivKeySize ...
+const PrivKeySize = 64
+
+// PrivKey ...
+type PrivKey []byte
+
+// Validate ...
+func (privKey PrivKey) Validate() error {
+	if len(privKey) != PrivKeySize {
+		return fmt.Errorf("must be exactly %d bytes", PrivKeySize)
+	}
+
+	return nil
+}
+
+// Serialize ...
+func (privKey PrivKey) Serialize() []byte {
+	return []byte(privKey)
+}
+
+// B64String ...
+func (privKey PrivKey) B64String() string {
+	return base64.StdEncoding.EncodeToString(privKey)
+}
+
+// PrivKeyFromB64String ...
+func PrivKeyFromB64String(s string) (PrivKey, error) {
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = PrivKey(decoded).Validate(); err != nil {
+		return nil, err
+	}
+
+	return decoded, nil
+}
+
+// HexString ...
+func (privKey PrivKey) HexString() string {
+	return hex.EncodeToString(privKey)
+}
+
+// PrivKeyFromHexString ...
+func PrivKeyFromHexString(s string) (PrivKey, error) {
+	decoded, err := hex.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = PrivKey(decoded).Validate(); err != nil {
+		return nil, err
+	}
+
+	return decoded, nil
+}
---
+++ pubkey.go
@@ -0,0 +1,65 @@
+package crypto
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+// PubKeySize ...
+const PubKeySize = 32
+
+// PubKey ...
+type PubKey []byte
+
+// Validate ...
+func (pubKey PubKey) Validate() error {
+	if len(pubKey) != PubKeySize {
+		return fmt.Errorf("must be exactly %d bytes", PubKeySize)
+	}
+
+	return nil
+}
+
+// Serialize ...
+func (pubKey PubKey) Serialize() []byte {
+	return []byte(pubKey)
+}
+
+// B64String ...
+func (pubKey PubKey) B64String() string {
+	return base64.StdEncoding.EncodeToString(pubKey)
+}
+
+// PubKeyFromB64String ...
+func PubKeyFromB64String(s string) (PubKey, error) {
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = PubKey(decoded).Validate(); err != nil {
+		return nil, err
+	}
+
+	return decoded, nil
+}
+
+// HexString ...
+func (pubKey PubKey) HexString() string {
+	return hex.EncodeToString(pubKey)
+}
+
+// PubKeyFromHexString ...
+func PubKeyFromHexString(s string) (PubKey, error) {
+	decoded, err := hex.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = PubKey(decoded).Validate(); err != nil {
+		return nil, err
+	}
+
+	return decoded, nil
+}
